a79b3a3b0707ff432003abdc42df8d1f35efbf69
Dilmurod200 <163502763+Dilmurod200@users.noreply.github.com>
2025-02-13 22:01:25 +0500

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,10 +1,10 @@
 import time
 
 def decorator_1(func):
-    def wrapper(*args, **kwargs):
-        start_time = time.time()
-        result = func(*args, **kwargs)
-        end_time = time.time()
-        print(f"Execution time of {func.__name__}: {end_time - start_time} seconds")
-        return result
-    return wrapper
+    def homework(*subjects, **grades):
+        start_study = time.time()
+        final_score = func(*subjects, **grades)
+        end_study = time.time()
+        print(f"Execution time of {func.name}: {end_study - start_study} seconds")
+        return final_score
+    return homework
