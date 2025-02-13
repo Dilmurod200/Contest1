b51879c1c6fc4b15a5e4f19e2ab36267b421c2da
Dilmurod200 <163502763+Dilmurod200@users.noreply.github.com>
2025-02-13 21:33:35 +0500

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,1 +1,10 @@
+import time
 
+def decorator_1(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"Execution time of {func.__name__}: {end_time - start_time} seconds")
+        return result
+    return wrapper
