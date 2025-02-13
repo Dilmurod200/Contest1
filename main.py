494bc5370d9dd6b3e414c004edfe199fa4826c8d
Dilmurod200 <163502763+Dilmurod200@users.noreply.github.com>
2025-02-13 22:02:01 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
-# main.py
-from your_module import kwargsAcceptFun, typeBasedTransformer, decorator_1
-
-kwargsAcceptFun(name="Dilmurod", age=20, city="Tashkent")
-
-data = {
-    "num": 9,
-    "text": "python",
-    "flag": True,
-    "items": [7, 8, 9],
-    "dict_data": {"alpha": 1, "beta": 2}
-}
-result = typeBasedTransformer(data)
-print(result)
+#donebyDilmurod
+import random
+from task1 import kwargsAcceptFun
+from task2 import typeBasedTransformer
+from task3 import decorator_1
 
 @decorator_1
-def demo_function():
-    time.sleep(2)
+def func():
+    print("I am ready to Start")
+    total_marks = 0
+    num_students = random.randint(10, 751)
+    for student in range(num_students):
+         total_marks += (student ** 2)
+    return total_marks 
 
-demo_function()
+@decorator_1
+def funx(num_students=None):
+    print("I am ready to do serious stuff")
+    highest_score = float('-inf')
+
+    if num_students is None:
+        num_students = random.randint(10, 751)
+
+    scores = [pow(student, 2) for student in range(num_students)]
+    for score in scores:
+        if score > highest_score:
+            highest_score = score
+    return highest_score 
+
+if name == "main":
+    func()
+    funx()
+    func()
+    funx()
+    func()
