1a56986b88ab101f934e8ee9ebef91868e1b6ad6
Dilmurod200 <163502763+Dilmurod200@users.noreply.github.com>
2025-02-13 21:31:22 +0500

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,1 +1,16 @@
+def typeBasedTransformer(data):
+    transformed_data = {}
+    
+    for key, value in data.items():
+        if isinstance(value, int) or isinstance(value, float):
+            transformed_data[key] = value ** 2  # Square numbers
+        elif isinstance(value, str):
+            transformed_data[key] = value[::-1]  # Reverse strings
+        elif isinstance(value, bool):
+            transformed_data[key] = not value  # Invert boolean
+        elif isinstance(value, list) or isinstance(value, tuple):
+            transformed_data[key] = value[::-1]  # Reverse list/tuple
+        elif isinstance(value, dict):
+            transformed_data[key] = {v: k for k, v in value.items()}  # Swap keys and values in dict
 
+    return transformed_data
